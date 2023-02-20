c43e022dd66b63d6d366f781cba076012ce25b1a
oscar640 <alexcaice85@gmail.com>
2023-02-20 16:45:52 -0500

    cambios en mi proyecto

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ addToCartBtn.addEventListener('click', ()=>{
 
     cartNotification.innerText = lastValue;
     cartNotification.style.display = 'block';
+    //priceModal.innerText = `$125 x${lastValue} <span>$${lastValue*125}.00</span>`
     drawProductInModal();
     
 });
@@ -39,12 +40,12 @@ addToCartBtn.addEventListener('click', ()=>{
 const cartIconBtn = document.querySelector('.header__cart');
 const cartModal = document.querySelector('.cart-modal');
 //let priceModal = document.querySelector('.cart-modal__price');
-const productContainer = document.querySelector('.cart-modal__checkout-container');
+const productContainer = document.querySelector('.cart-modal__chekout-container');
 
 cartIconBtn.addEventListener('click', ()=>{
     cartModal.classList.toggle('show');
 
-    if (lastValue == 0){
+    if (lastValue === 0){
         productContainer.innerHTML = '<p class="cart-empty">Your cart is empty</p>';
     
     }else{
@@ -71,43 +72,98 @@ let imageIndex = 1;
 
 nextGalleryBtn.addEventListener('click', ()=>{
     changeNextImage(imageContainer);
-})
+});
 
 previusGalleryBtn.addEventListener('click', ()=>{
     changePreviusImage(imageContainer);
-})
+});
 
 //mostrar el modal de imagenes cuando hago click en la imagen principal.
 const imagesModal = document.querySelector('.modal-gallery__background');
+const closeModalBtn = document.querySelector('.modal-gallery__close');
 
 imageContainer.addEventListener('click', ()=>{
     imagesModal.style.display = 'grid';
 });
 
+closeModalBtn.addEventListener('click', ()=>{
+    imagesModal.style.display = 'none';
+});
+
+// cambiar las imagenes principales desde thumbnails
+ let thumbnails = document.querySelectorAll('.gallery__thumnail')
+ thumbnails = [...thumbnails]
+
+ thumbnails.forEach(thumbnail =>{
+     thumbnail.addEventListener('click', event=>{
+       console.log(event.target.id)
+       imageContainer.style.backgroundImage = `url('../ecommerce-product-page-main/images/image-product-${event.target.id}.jpg')`
+     });
+ });
+
+// cambiar las imagenes principales desde thumbnails
+let modalthumbnails = document.querySelectorAll('.modal-gallery__thumnail');
+const modalImageContainer = document.querySelector('.modal-gallery__image-container');
+modalthumbnails =[...modalthumbnails]
 
 
+modalthumbnails.forEach(modalthumbnail => {
+    modalthumbnail.addEventListener('click', event=>{
+        console.log(event.target.id.slice(-1))
+        modalImageContainer.style.backgroundImage = `url('../ecommerce-product-page-main/images/image-product-${event.target.id.slice(-1)}.jpg')`
 
+    });
+});
+
+//cambiar imagenen principla del modal desde flechas en el modal
+const previusModalBtn = document.querySelector('.modal-gallery__previus');
+const nextModalBtn = document.querySelector('.modal-gallery__next');
+
+nextModalBtn.addEventListener('click', ()=>{
+    changeNextImage(modalImageContainer);
+});
+
+previusModalBtn.addEventListener('click', ()=>{
+    changePreviusImage(modalImageContainer);
+});
+
+//mostrar el navbar cuando preciono el menu de hamburguesa
+const hamburgerMenu = document.querySelector('.header__menu');
+const modalNavbar = document.querySelector('.modal-navbar__background');
+const closeModalNavbar = document.querySelector('.modal-navbar__close-icon');
+
+modalNavbar.style.display = 'none'
+
+hamburgerMenu.addEventListener('click', ()=>{
+    console.log('abrir modal');
+    modalNavbar.style.display = 'block';
+});
+
+closeModalNavbar.addEventListener('click', ()=>{
+    modalNavbar.style.display = 'none';
+});                                                                                            
+                           
 
 //funciones 
 
 function drawProductInModal(){
     productContainer.innerHTML = `
         <div class="cart-modal__details-container">
-            <img class="cart-modal__image" src="./images/image-product-1-thumbnail.jpg" alt="">
+            <img class="cart-modal__image" src="./ecommerce-product-page-main/images/image-product-1-thumbnail.jpg" alt="">
             <div>
             <p class="cart-modal__product">Autumn Limited Edition...</p>
             <p class="cart-modal__price">$125 x3 <span>$375.00</span> </p>
             </div>
-            <img class="cart-modal__delete" src="./images/icon-delete.svg" alt="delete">
+            <img class="cart-modal__delete" src="./ecommerce-product-page-main/images/icon-delete.svg" alt="delete">
         </div>
-        <button class="cart-modal__chekount" >Checkout</button>`
+        <button class="cart-modal__chekount" >Checkout</button>`;
     deleteProduct()
     let priceModal = document.querySelector('.cart-modal__price');
     priceModal.innerHTML = `$125 x${lastValue} <span>$${lastValue*125}.00</span>`;
 }
 
 function changeNextImage(imgcontainer){
-    if(imageIndex == 4){
+    if(imageIndex === 4){
         imageIndex = 1;
     }else{
         imageIndex++;
@@ -116,7 +172,7 @@ function changeNextImage(imgcontainer){
 }
 
 function changePreviusImage(imgcontainer){
-    if(imageIndex == 1){
+    if(imageIndex === 1){
         imageIndex = 4;
     }else{
         imageIndex--;
